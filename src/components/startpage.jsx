a7020025cd7c9813ed7ee602fa9d18d005f75e7e
Rîmbu Tudor-Gabriel <rtudorg2003@gmail.com>
2025-03-05 10:28:52 +0200

    Add minimal phone design

--- src/components/startpage.jsx
+++ src/components/startpage.jsx
@@ -18,23 +18,25 @@ export default function Startpage() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-row items-center h-full w-full gap-4"
+      className="flex sm:flex-row flex-col items-center h-full w-full gap-4"
     >
       <input
         type="text"
         placeholder="stop coding"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className="border h-full w-96 border-foreground rounded p-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-sky-500 text-sky-500"
+        className="border h-full w-full min-h-12 sm:w-96 border-foreground rounded p-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-sky-500 text-sky-500"
       />
-      <Link
-        className="inline-block h-full rounded bg-foreground text-background px-4 py-2 hover:bg-sky-500 hover:cursor-pointer transition-all place-content-center"
-        href={`/${username}`}
-      >
-        Submit!
-      </Link>
-      <div>
-        <ThemeSwitch />
+      <div className="flex flex-row items-center h-full justify-center gap-2">
+        <Link
+          className="inline-block h-full rounded bg-foreground text-background px-4 py-2 hover:bg-sky-500 hover:cursor-pointer transition-all place-content-center"
+          href={`/${username}`}
+        >
+          Submit!
+        </Link>
+        <div>
+          <ThemeSwitch />
+        </div>
       </div>
     </form>
   );
